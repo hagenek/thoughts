6e4e8d549011df279df992b3c447aa1d68c014ff
Georg H. Ekeberg <georghagen@gmail.com>
2021-03-24 18:46:04 +0100

    added scaffolding for a simple twitter-clone app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Thoughts />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Thoughts from "./components/Thoughts.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Thoughts,
+  },
+};
 </script>
 
 <style>
